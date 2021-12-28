534cf95917eb6685fc08dbfca24fca463b28f28f
root <root@ls.pqmdrm2z>
2021-12-28 14:44:22 +0800

    my first commit

---
+++ Student.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private Integer id;
+    private String name;
+    private String age;
+
+    public Student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Student() {
+
+    }
+
+
+}
