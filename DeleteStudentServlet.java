b823ea0fc0ea4a2cd5259d47b989fd82df4ee725
root <root@ls.pqmdrm2z>
2021-12-28 15:00:47 +0800

    deleteStudent

--- DeleteStudentServlet.java
+++ DeleteStudentServlet.java
@@ -1,6 +1,5 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,20 +16,14 @@ import java.util.Map;
 
 @WebServlet("/deleteStudent")
 public class DeleteStudentServlet extends HttpServlet {
-
     private Connection connection = null;
-
     private Gson gson = new Gson();
-
     public void init() {
         try {
-            // 自动注册加载
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             String url = "jdbc:mysql://106.12.157.37:3306/linux_final?serverTimezone=GMT";
             String user = "root";
             String password = "83387330Znn!";
-
             connection = DriverManager.getConnection(url, user, password);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -50,9 +43,7 @@ public class DeleteStudentServlet extends HttpServlet {
         response.setCharacterEncoding("utf-8");
         response.setContentType("application/json;charset=utf-8");
         Map<String, Object> result = new HashMap<String, Object>();
-
         deleteStudent(Integer.parseInt(getRequestBody(request).get("id")));
-
         response.getWriter().print("删除成功");
         response.getWriter().close();
     }
@@ -76,7 +67,6 @@ public class DeleteStudentServlet extends HttpServlet {
 
     private void deleteStudent(int id) {
         try {
-            // sql预编译
             PreparedStatement ps = connection.prepareStatement("delete from student where id = ?");
             ps.setInt(1, id);
             ps.execute();
